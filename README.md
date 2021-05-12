e8f26715959e7ccd7d330f0e595f6e6c9210b81e
Greg P <gplechaty@gmail.com>
2021-05-11 22:47:22 -0600

    removed console.logs, removed excess spacing in code, lightly modified the readme file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# currencyCompare
-SPA of currency comparisons
+# Exchange Rate Display #
+
+##### In it's final state, this project will display currency conversions across sevaral countries. #####
+
+###### Notes: ######
+- Data is provided by https://exchangerate.host/.
+- Bar chart represents proportional value of each currency.
+- The page returns monthly currency exchange rates against the Euro. Essentially, how much of a given currency is needed to purchase 1 Euro.
+- Three inputs are required: Start Month, Number of months to display, and currency. 
--- js/main.js
+++ js/main.js
@@ -1,26 +1,16 @@
-console.log('main.js loaded');
-
-
-//for now, stores an "Object", aka a dictionary, of the rates as of a given date
-
 let stateObjects = [];
-
 
 function calculateDateArray() {
     let startMonth = document.querySelector('#start_month').value;
     let startYear = document.querySelector('#start_year').value;
     let startDate = new Date(startYear, startMonth, 1);
-    //let startDate = startYear + '-' + startMonth + '-01'
     let numOfMonths = document.querySelector('#numOfMonths').value;
     let dateArray = [];
     for (let i = 0; i < numOfMonths; i++) {
         dateArray.push(new Date (startDate));
-        //console.log('dateArray:', dateArray)
         startDate.setMonth(startDate.getMonth() + 1);
       }
-
-
-     let dateArrayFormatted = [];
+    let dateArrayFormatted = [];
     for (let date of dateArray) {
         let year = date.getFullYear();
         let month = date.getMonth() + 1;
@@ -31,30 +21,22 @@ function calculateDateArray() {
         if (day < 10) {
             day = '0' + day.toString();
         }
-        
         dateArrayFormatted.push(year + '-' + month + '-' + day);
     };
-
-    console.log('dateArrayFormatted: ', dateArrayFormatted)
     return dateArrayFormatted; 
 };
 
 function calculateAddBarPercent() {
-    console.log('function: calculateAddBarPercent')
-    //Get array of rates
     let rateArray = []
     for (let item of stateObjects) {
         rateArray.push(item.rate)
     };
-    console.log('rateArray: ', rateArray)
     let greatestRate = Math.max(...rateArray);
-    console.log('greatestRate: ', greatestRate)
     for (let item of stateObjects) {
         let barPercent = item.rate /  greatestRate * 100;
         item['percent'] = barPercent;
     };
 };
-
 
 function createBar() {
     //TODO: this creates the graph 3 times. This is not efficient.
@@ -72,10 +54,8 @@ function createBar() {
         barPara = document.createElement('p');
         barPara.textContent = item.date + ': The value of ' + item.currency + ' is '+ item.rate;
         barText.appendChild(barPara);
-        // Set Alert. OOOORRRR the hover appear thing from solution, ideally
         container.appendChild(bar);
     }
-
 };
 
 function doFetch() {
@@ -83,10 +63,6 @@ function doFetch() {
     let container = document.querySelector('.ExchangeRateChart-display')
     container.innerHTML = '';
     stateObjects = [];
-    //let search = document.querySelector('#search').value
-    //console.log('search: ', search)
-    // TODO: This is where I add calculateDateArray
-    //let dates = ['2020-04-04','2020-03-04','2020-02-04', '2020-01-04'];
     let dates = calculateDateArray();
     for (let date of dates) {
         console.log('date:', date);
@@ -97,66 +73,18 @@ function doFetch() {
         .then(response => response.json())
         .then(data => {
             console.log('data.rates:', data.rates[ticker]);
-            ////////////////// THIS IS WHAT WE WANT TO MODIFY
-            //state.rates.push(data.rates.USD);
-            // new one:
-            //let barPercent = data
             barObject = {
                 'date': date,
                 'currency': ticker,
                 'rate': data.rates[ticker]
             }
             stateObjects.push(barObject);
-            ////////////
             console.log('stateObjects:', stateObjects);
-            // Add this render function back eventually render();
-            //for (let rate of state.Object) {
-            //    console.log(rate);
-            //};
+
             render()
         })
     };
-
-
-
-
 };
-/*
-function doFetchFancy() {
-    console.log('function: doFetchFancy')
-    let container = document.querySelector('.ExchangeRateChart-display')
-    container.innerHTML = '';
-    state.rates = [];
-    //let search = document.querySelector('#search').value
-    //console.log('search: ', search)
-    let dates = ['2020-04-04','2020-03-04','2020-02-04',];
-    const promise1 = fetch('https://api.exchangerate.host/2020-04-04?symbols=USD')
-        .then(response => response.json())
-        .then(data => {
-            console.log('data.rates:', data.rates.USD);
-            state.rates.push(data.rates.USD);
-            console.log('state.rates:', state.rates);
-        });
-    const promise2 = fetch('https://api.exchangerate.host/2020-03-04?symbols=USD')
-        .then(response => response.json())
-        .then(data => {
-            console.log('data.rates:', data.rates.USD);
-            state.rates.push(data.rates.USD);
-            console.log('state.rates:', state.rates);
-        });
-    const promise3 =  fetch('https://api.exchangerate.host/2020-02-04?symbols=USD')
-        .then(response => response.json())
-        .then(data => {
-            console.log('data.rates:', data.rates.USD);
-            state.rates.push(data.rates.USD);
-            console.log('state.rates:', state.rates);
-        });
-    Promise.all([promise1, promise2, promise3]).then(render());
-   
-
-    
-};
-*/
 
 function render() {
     console.log('function: render');
